15113b22c7b81de2f1d9fe6f613080fe1a3f8c80
Stevon Wright <stevonisepic@gmail.com>
2022-09-24 22:54:06 -0400

    Reset migrations, added comment fields

--- prisma/migrations/20220918024237_add_local_user/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "LocalUser" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "LocalUser_name_key" ON "LocalUser"("name");
