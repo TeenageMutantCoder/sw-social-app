a50b0990662d52af18a84dafef874ba8f01de6a8
TeenageMutantCoder <stevonisepic@gmail.com>
2023-07-09 07:59:37 -0400

    Run new-link codemod

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,10 +10,12 @@ const Navbar = () => {
   return (
     <nav className="mb-3 bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 sticky w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <Link href="/">
-          <a className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+        <Link
+          href="/"
+          className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          
             Social App
-          </a>
+          
         </Link>
         <div className="flex md:order-2">
           {status === 'authenticated' && (
